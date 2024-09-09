3855e69062151079dc49af74a49bb4d035695336
keyla <ktacuabe@udc.edu.ar>
2024-09-09 11:23:14 -0300

    se creó el modelo Mensaje y se agregó la app mensajes a los settings del proyecto

--- mensajes/models.py
+++ mensajes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Mensaje(models.Model):
+    texto = models.TextField(default='hola')
+    remitente = models.CharField(max_length=25, default='keyla')
+    destinatario = models.CharField(max_length=25, default='keyla')
+    fecha_y_hora = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.texto
+
